bae573c10941f9d20ee7134a69cb2524930ef754
Glenn <33450392+glenn2223@users.noreply.github.com>
2021-06-03 12:52:27 +0100

    Lint & format work
    Formatted the file
    Made some changes based on linting warnings/errors

--- src/Index.ts
+++ src/Index.ts
@@ -1,278 +1,262 @@
-import { HoverPosition } from "@topmarksdevelopment/hover-position";
-import HoverOptions from "./Interfaces/HoverOptions";
+import { HoverPosition } from '@topmarksdevelopment/hover-position';
+import HoverOptions from './Interfaces/HoverOptions';
 
 export class HoverBox {
-    private _listening: boolean = false;
-    private _selector: string;
-    private _options: HoverOptions;
-    private _timeouts: { [key: string]: ReturnType<typeof setTimeout> | null };
+	private _listening = false;
+	private _selector: string;
+	private _options: HoverOptions;
+	private _timeouts: { [key: string]: ReturnType<typeof setTimeout> | null };
 
-    /**
-     * Setup hoverbox events and start listening
-     * @param selector The selector to find the child that contains the text to show
-     * @param defaultOptions The default options to apply
-     */
-    constructor(selector: string, defaultOptions?: HoverOptions) {
-        this._selector = selector;
-        this._options = Object.assign({}, defaultOptions);
-        this._timeouts = {};
+	/**
+	 * Setup hoverbox events and start listening
+	 * @param selector The selector to find the child that contains the text to show
+	 * @param defaultOptions The default options to apply
+	 */
+	constructor(selector: string, defaultOptions?: HoverOptions) {
+		this._selector = selector;
+		this._options = Object.assign({}, defaultOptions);
+		this._timeouts = {};
 
-        this.startListening();
-    }
+		this.startListening();
+	}
 
-    /** # Public methods */
+	/** # Public methods */
 
-    /**
-     * Start listening for hoverbox events
-     */
-    startListening() {
-        if (!this._listening) {
-            const that = this;
+	/**
+	 * Start listening for hoverbox events
+	 */
+	startListening(): void {
+		if (!this._listening) {
+			const that = this;
 
-            HoverBox.addHoverListener("mouseenter", that._selector, function (this: HTMLElement) {
-                that.showHoverBox(this);
-            });
-            HoverBox.addHoverListener("mouseleave", that._selector, function (this: HTMLElement) {
-                that.hideHoverBox(this);
-            });
+			HoverBox.addHoverListener('mouseenter', that._selector, function (this: HTMLElement) {
+				that.showHoverBox(this);
+			});
+			HoverBox.addHoverListener('mouseleave', that._selector, function (this: HTMLElement) {
+				that.hideHoverBox(this);
+			});
 
-            this._listening = true;
-        }
-    }
+			this._listening = true;
+		}
+	}
 
-    /**
-     * Stop listening for hoverbox events
-     */
-    stopListening() {
-        if (this._listening) {
-            const that = this;
+	/**
+	 * Stop listening for hoverbox events
+	 */
+	stopListening(): void {
+		if (this._listening) {
+			const that = this;
 
-            HoverBox.removeHoverListener(
-                "mouseenter",
-                that._selector,
-                function (this: HTMLElement) {
-                    that.showHoverBox(this);
-                }
-            );
-            HoverBox.removeHoverListener(
-                "mouseleave",
-                that._selector,
-                function (this: HTMLElement) {
-                    that.hideHoverBox(this);
-                }
-            );
+			HoverBox.removeHoverListener('mouseenter', that._selector, function (this: HTMLElement) {
+				that.showHoverBox(this);
+			});
+			HoverBox.removeHoverListener('mouseleave', that._selector, function (this: HTMLElement) {
+				that.hideHoverBox(this);
+			});
 
-            this._listening = false;
-        }
-    }
+			this._listening = false;
+		}
+	}
 
-    /** # Private methods */
+	/** # Private methods */
 
-    /** ## HoverBox handlers & fetchers */
+	/** ## HoverBox handlers & fetchers */
 
-    private handleHoverBoxAnimation(t: HTMLElement, showOrHide: "show" | "hide"): void {
-        const hoverBox = this.getHoverBox(t, showOrHide);
+	private handleHoverBoxAnimation(t: HTMLElement, showOrHide: 'show' | 'hide'): void {
+		const hoverBox = this.getHoverBox(t, showOrHide);
 
-        if (showOrHide === "show") {
-            const tId = `${hoverBox.id}_${showOrHide}`;
+		if (hoverBox === null) {
+			return;
+		}
 
-            hoverBox.style.display = "none";
+		if (showOrHide === 'show') {
+			const tId = `${hoverBox.id}_${showOrHide}`;
 
-            const pos = new HoverPosition({
-                my: this._options.setMy ?? "top center",
-                at: this._options.at ?? "bottom center",
-                anchor: t,
-                target: hoverBox,
-                collision: this._options?.collision,
-                bestFitPreference: this._options?.bestFitPreference,
-                defaults: this._options?.defaults,
-            });
+			hoverBox.style.display = 'none';
 
-            // apply new position
-            hoverBox.style.top = pos.top;
-            hoverBox.style.left = pos.left;
+			const pos = new HoverPosition({
+				my: this._options.setMy ?? 'top center',
+				at: this._options.at ?? 'bottom center',
+				anchor: t,
+				target: hoverBox,
+				collision: this._options?.collision,
+				bestFitPreference: this._options?.bestFitPreference,
+				defaults: this._options?.defaults,
+			});
 
-            let timeoutId = this._timeouts[tId];
+			// apply new position
+			hoverBox.style.top = pos.top;
+			hoverBox.style.left = pos.left;
 
-            if (timeoutId && timeoutId !== null) {
-                this._timeouts[tId] = null;
+			let timeoutId = this._timeouts[tId];
 
-                clearTimeout(timeoutId);
-            } else {
-                timeoutId = setTimeout(() => {
-                    this._timeouts[tId] = null;
+			if (timeoutId && timeoutId !== null) {
+				this._timeouts[tId] = null;
 
-                    hoverBox.dataset.state = "shown";
-                    hoverBox.style.display = "block";
-                    hoverBox.style.opacity = "1";
-                }, this._options.transitionDelay ?? 333);
+				clearTimeout(timeoutId);
+			} else {
+				timeoutId = setTimeout(() => {
+					this._timeouts[tId] = null;
 
-                this._timeouts[tId] = timeoutId;
-            }
-        } else {
-            this.registerHideTimeout(hoverBox);
-        }
-    }
+					hoverBox.dataset.state = 'shown';
+					hoverBox.style.display = 'block';
+					hoverBox.style.opacity = '1';
+				}, this._options.transitionDelay ?? 333);
 
-    private getHoverBox(t: HTMLElement, showOrHide: "show" | "hide"): HTMLElement | null {
-        const inlineData: HoverOptions & { hoverData?: string } = Object.assign(
-            this._options,
-            t.dataset
-        );
+				this._timeouts[tId] = timeoutId;
+			}
+		} else {
+			this.registerHideTimeout(hoverBox);
+		}
+	}
 
-        if (inlineData.hoverData ?? t.querySelectorAll(".HoverData").item(0)) {
-            let foundHoverBox: HTMLElement | null = null;
+	private getHoverBox(t: HTMLElement, showOrHide: 'show' | 'hide'): HTMLElement | undefined {
+		const inlineData: HoverOptions & { hoverData?: string } = Object.assign(this._options, t.dataset);
 
-            if (t.dataset?.hoverBoxId) {
-                foundHoverBox = document.getElementById(t.dataset.hoverBoxId);
-            }
+		if (inlineData.hoverData ?? t.querySelectorAll('.HoverData').item(0)) {
+			let foundHoverBox: HTMLElement = null;
 
-            if (foundHoverBox) {
-                // Dont fire the same event again
-                if (
-                    (foundHoverBox.dataset.state === "shown" && showOrHide === "show") ||
-                    (foundHoverBox.dataset.state === "hidden" && showOrHide === "hide")
-                ) {
-                    const idToFind = `${foundHoverBox.id}_${
-                            showOrHide === "hide" ? "show" : "hide"
-                        }`,
-                        timeoutId = this._timeouts[idToFind];
+			if (t.dataset?.hoverBoxId) {
+				foundHoverBox = document.getElementById(t.dataset.hoverBoxId);
+			}
 
-                    if (timeoutId && timeoutId !== null) {
-                        this._timeouts[idToFind] = null;
+			if (foundHoverBox) {
+				// Dont fire the same event again
+				if (
+					(foundHoverBox.dataset.state === 'shown' && showOrHide === 'show') ||
+					(foundHoverBox.dataset.state === 'hidden' && showOrHide === 'hide')
+				) {
+					const idToFind = `${foundHoverBox.id}_${showOrHide === 'hide' ? 'show' : 'hide'}`,
+						timeoutId = this._timeouts[idToFind];
 
-                        clearTimeout(timeoutId);
-                    }
+					if (timeoutId && timeoutId !== null) {
+						this._timeouts[idToFind] = null;
 
-                    return;
-                }
+						clearTimeout(timeoutId);
+					}
 
-                return foundHoverBox;
-            }
+					return undefined;
+				}
 
-            return this.createHoverBox(t, inlineData);
-        }
-    }
+				return foundHoverBox;
+			}
 
-    private createHoverBox(
-        t: HTMLElement,
-        inlineData: HoverOptions & { hoverData?: string | undefined }
-    ) {
-        const id = "hB_" + Math.random().toString().substr(2);
+			return this.createHoverBox(t, inlineData);
+		}
 
-        t.dataset.hoverBoxId = id;
+		return undefined;
+	}
 
-        const hoverBox = document.createElement("div");
-        hoverBox.id = id;
-        hoverBox.classList.add("HoverData");
-        hoverBox.dataset.state = "shown";
+	private createHoverBox(t: HTMLElement, inlineData: HoverOptions & { hoverData?: string | undefined }): HTMLElement {
+		const id = 'hB_' + Math.random().toString().substr(2);
 
-        if (this._options.allowHtml) {
-            hoverBox.innerHTML =
-                inlineData.hoverData ?? t.querySelectorAll(".HoverData").item(0)?.innerHTML ?? "";
-        } else {
-            hoverBox.innerText =
-                inlineData.hoverData ?? t.querySelectorAll(".HoverData").item(0)?.textContent ?? "";
-        }
+		t.dataset.hoverBoxId = id;
 
-        document.body.insertAdjacentElement("beforeend", hoverBox);
+		const hoverBox = document.createElement('div');
+		hoverBox.id = id;
+		hoverBox.classList.add('HoverData');
+		hoverBox.dataset.state = 'shown';
 
-        if (this._options.keepOpen ?? true) {
-            const that = this;
+		if (this._options.allowHtml) {
+			hoverBox.innerHTML = inlineData.hoverData ?? t.querySelectorAll('.HoverData').item(0)?.innerHTML ?? '';
+		} else {
+			hoverBox.innerText = inlineData.hoverData ?? t.querySelectorAll('.HoverData').item(0)?.textContent ?? '';
+		}
 
-            hoverBox.addEventListener("mouseenter", function (this: HTMLElement) {
-                let timeoutId = that._timeouts[`${hoverBox.id}_hide`];
+		document.body.insertAdjacentElement('beforeend', hoverBox);
 
-                if (timeoutId && timeoutId !== null) {
-                    that._timeouts[`${hoverBox.id}_hide`] = null;
+		if (this._options.keepOpen ?? true) {
+			const that = this;
 
-                    clearTimeout(timeoutId);
-                }
-            });
-            hoverBox.addEventListener("mouseleave", function (this: HTMLElement) {
-                that.registerHideTimeout(this);
-            });
-        }
+			hoverBox.addEventListener('mouseenter', function (this: HTMLElement) {
+				const timeoutId = that._timeouts[`${hoverBox.id}_hide`];
 
-        return hoverBox;
-    }
+				if (timeoutId && timeoutId !== null) {
+					that._timeouts[`${hoverBox.id}_hide`] = null;
 
-    /** ## Timeout handlers */
+					clearTimeout(timeoutId);
+				}
+			});
+			hoverBox.addEventListener('mouseleave', function (this: HTMLElement) {
+				that.registerHideTimeout(this);
+			});
+		}
 
-    private registerHideTimeout(hoverBox: HTMLElement) {
-        let timeoutId = this._timeouts[`${hoverBox.id}_hide`];
+		return hoverBox;
+	}
 
-        if (timeoutId && timeoutId !== null) {
-            this._timeouts[`${hoverBox.id}_hide`] = null;
+	/** ## Timeout handlers */
 
-            clearTimeout(timeoutId);
-        } else {
-            timeoutId = setTimeout(() => {
-                hoverBox.dataset.state = "hidden";
-                hoverBox.style.opacity = "0";
+	private registerHideTimeout(hoverBox: HTMLElement) {
+		let timeoutId = this._timeouts[`${hoverBox.id}_hide`];
 
-                this._timeouts[`${hoverBox.id}_hide`] = setTimeout(() => {
-                    this._timeouts[`${hoverBox.id}_hide`] = null;
+		if (timeoutId && timeoutId !== null) {
+			this._timeouts[`${hoverBox.id}_hide`] = null;
 
-                    hoverBox.style.display = "none";
-                }, this._options.transitionDuration ?? 333);
-            }, this._options.transitionDelay ?? 333);
+			clearTimeout(timeoutId);
+		} else {
+			timeoutId = setTimeout(() => {
+				hoverBox.dataset.state = 'hidden';
+				hoverBox.style.opacity = '0';
 
-            this._timeouts[`${hoverBox.id}_hide`] = timeoutId;
-        }
-    }
+				this._timeouts[`${hoverBox.id}_hide`] = setTimeout(() => {
+					this._timeouts[`${hoverBox.id}_hide`] = null;
 
-    /** ## Event handlers */
+					hoverBox.style.display = 'none';
+				}, this._options.transitionDuration ?? 333);
+			}, this._options.transitionDelay ?? 333);
 
-    private showHoverBox(el: HTMLElement) {
-        this.handleHoverBoxAnimation(el, "show");
-    }
+			this._timeouts[`${hoverBox.id}_hide`] = timeoutId;
+		}
+	}
 
-    private hideHoverBox(el: HTMLElement) {
-        this.handleHoverBoxAnimation(el, "hide");
-    }
+	/** ## Event handlers */
 
-    private static addHoverListener(
-        eventType: string,
-        delegateSelector: string,
-        handler: Function
-    ) {
-        document.addEventListener(
-            eventType,
-            function (this: HTMLElement, e) {
-                const target = <HTMLElement>e.target;
+	private showHoverBox(el: HTMLElement) {
+		this.handleHoverBoxAnimation(el, 'show');
+	}
 
-                if (target && target !== this && target.matches(delegateSelector)) {
-                    handler.call(target, e);
-                }
-            },
-            true
-        );
-    }
+	private hideHoverBox(el: HTMLElement) {
+		this.handleHoverBoxAnimation(el, 'hide');
+	}
 
-    private static removeHoverListener(
-        eventType: string,
-        delegateSelector: string,
-        handler: Function
-    ) {
-        document.removeEventListener(
-            eventType,
-            function (this: HTMLElement, e) {
-                const target = <HTMLElement>e.target;
+	private static addHoverListener(eventType: string, delegateSelector: string, handler: (this: HTMLElement) => void) {
+		document.addEventListener(
+			eventType,
+			function (this: HTMLElement, e) {
+				const target = <HTMLElement>e.target;
 
-                if (target && target !== this && target.matches(delegateSelector)) {
-                    handler.call(target, e);
-                }
-            },
-            true
-        );
-    }
+				if (target && target !== this && target.matches(delegateSelector)) {
+					handler.call(target, e);
+				}
+			},
+			true,
+		);
+	}
 
-    /** ## Unusable `.animate` (for now) */
+	private static removeHoverListener(
+		eventType: string,
+		delegateSelector: string,
+		handler: (this: HTMLElement) => void,
+	) {
+		document.removeEventListener(
+			eventType,
+			function (this: HTMLElement, e) {
+				const target = <HTMLElement>e.target;
 
-    // Can't use it - see: https://bugs.chromium.org/p/chromium/issues/detail?id=1141935&q=&can=5
-    // Note: `play` or `reverse` depends on `playspeed`?? which is 1 or -1
-    /*private setupAnimation(hoverBox: HTMLElement) {
+				if (target && target !== this && target.matches(delegateSelector)) {
+					handler.call(target, e);
+				}
+			},
+			true,
+		);
+	}
+
+	/** ## Unusable `.animate` (for now) */
+
+	// Can't use it - see: https://bugs.chromium.org/p/chromium/issues/detail?id=1141935&q=&can=5
+	// Note: `play` or `reverse` depends on `playspeed`?? which is 1 or -1
+	/*private setupAnimation(hoverBox: HTMLElement) {
         const anim =
             hoverBox.animate(
                 [{ ["opacity"]: 0 }, { ["opacity"]: 1 }],
