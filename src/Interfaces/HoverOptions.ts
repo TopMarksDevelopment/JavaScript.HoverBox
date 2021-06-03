da9b1d67a1a45f07687e6c8dcb55ddd6ab1016f6
Glenn <33450392+glenn2223@users.noreply.github.com>
2021-06-03 12:51:52 +0100

    Formatted

--- src/Interfaces/HoverOptions.ts
+++ src/Interfaces/HoverOptions.ts
@@ -1,15 +1,15 @@
-import { CombinedAlignment, PositionAlignment, PositionCollision } from "@topmarksdevelopment/hover-position";
+import { CombinedAlignment, PositionAlignment, PositionCollision } from '@topmarksdevelopment/hover-position';
 
 export default interface HoverOptions {
-    setMy?: PositionAlignment;
-    at?: PositionAlignment;
-    keepOpen?: boolean;
-    allowHtml?: boolean;
-    transitionDelay?: number;
-    transitionDuration?: number;
+	setMy?: PositionAlignment;
+	at?: PositionAlignment;
+	keepOpen?: boolean;
+	allowHtml?: boolean;
+	transitionDelay?: number;
+	transitionDuration?: number;
 
-    // HoverPosition specific
-    collision?: PositionCollision;
-    bestFitPreference?: "horizontal" | "vertical";
-    defaults?: { my: CombinedAlignment; at: CombinedAlignment };
+	// HoverPosition specific
+	collision?: PositionCollision;
+	bestFitPreference?: 'horizontal' | 'vertical';
+	defaults?: { my: CombinedAlignment; at: CombinedAlignment };
 }
